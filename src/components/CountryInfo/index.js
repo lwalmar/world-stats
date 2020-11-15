6e0dd79db6e6460de03cf1fa9ac74e1a756306b7
lwalmar <lwalmar@mail.ru>
2020-11-15 15:49:07 +0300

    connect StackChart

--- src/components/CountryInfo/index.js
+++ src/components/CountryInfo/index.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
 import * as worldProfits from './data/worldProfits.json';
 import * as worldCountries from '../Map/data/world_countries.json';
+import StackChart from '../BarChart';
 import './styles.css';
 
 const getCountryProfitData = (countryId) => {
-  return worldProfits.default[countryId];
+  const countryProfits = worldProfits.default[countryId];
+
+  return countryProfits
+    ? countryProfits.map((profitsByPeriod) => (
+      [
+        {y: profitsByPeriod.profits},
+        {y: profitsByPeriod.dividendsAndBuybacks},
+        {y: profitsByPeriod.netDebtOfHouseholdsAndNISH},
+        {y: profitsByPeriod.netDebtOfGeneralGovernment},
+        {y: profitsByPeriod.fixedAssetsofDomesticBusiness},
+        {y: profitsByPeriod.currentAmount},
+        {y: profitsByPeriod.otherFactors},
+        {y: profitsByPeriod.discrepancies}
+      ]
+    ))
+    : [];
 };
 const getCountryName = (countryId) => {
   const countryInfo = worldCountries.default.features.find((item) => item.id === countryId);
@@ -22,6 +38,13 @@ export const CountryInfo = ({
     <div className="countryInfo">
       <div className='countryInfo_header'>
         {countryName}
+      </div>
+      <div>
+        <StackChart
+          data={countryProfitData}
+          width={700}
+          height={400}
+        />
       </div>
     </div>
 )};
