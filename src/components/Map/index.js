c01d9562bab212107e9d7dd8346bbeb9cc5ea11d
lwalmar <lwalmar@mail.ru>
2020-11-18 21:03:52 +0300

    connect map tip

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import * as d3 from "d3";
+import {getMapTip} from "../../modules/map-tip.js";
 import * as topojson from "topojson";
 import * as worldCountries from './data/world_countries.json';
+import './styles.css';
 
 class Map extends React.Component {
   componentDidMount() {
@@ -15,13 +17,92 @@ class Map extends React.Component {
     }
   }
 
-  async updateChart() {
+  getTip () {
+    // Set tooltips
+    const tip = getMapTip()(c)
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(
+        d => {
+          console.log('d', d)
+          return `<strong>Country: </strong><span class='details'>${
+            d.properties.name
+            }<br></span><strong>Profits: </strong><span class='details'>${d.profits}</span>`
+        })
+
+    tip.direction(function(d) {
+      console.log('d', d)
+      if (d.properties.name === 'Antarctica') return 'n'
+      // Americas
+      if (d.properties.name === 'Greenland') return 's'
+      if (d.properties.name === 'Canada') return 'e'
+      if (d.properties.name === 'USA') return 'e'
+      if (d.properties.name === 'Mexico') return 'e'
+      // Europe
+      if (d.properties.name === 'Iceland') return 's'
+      if (d.properties.name === 'Norway') return 's'
+      if (d.properties.name === 'Sweden') return 's'
+      if (d.properties.name === 'Finland') return 's'
+      if (d.properties.name === 'Russia') return 'w'
+      // Asia
+      if (d.properties.name === 'China') return 'w'
+      if (d.properties.name === 'Japan') return 's'
+      // Oceania
+      if (d.properties.name === 'Indonesia') return 'w'
+      if (d.properties.name === 'Papua New Guinea') return 'w'
+      if (d.properties.name === 'Australia') return 'w'
+      if (d.properties.name === 'New Zealand') return 'w'
+      // otherwise if not specified
+      return 'n'
+    })
+
+    tip.offset(function(d) {
+      // [top, left]
+      if (d.properties.name === 'Antarctica') return [0, 0]
+      // Americas
+      if (d.properties.name === 'Greenland') return [10, -10]
+      if (d.properties.name === 'Canada') return [24, -28]
+      if (d.properties.name === 'USA') return [-5, 8]
+      if (d.properties.name === 'Mexico') return [12, 10]
+      if (d.properties.name === 'Chile') return [0, -15]
+      // Europe
+      if (d.properties.name === 'Iceland') return [15, 0]
+      if (d.properties.name === 'Norway') return [10, -28]
+      if (d.properties.name === 'Sweden') return [10, -8]
+      if (d.properties.name === 'Finland') return [10, 0]
+      if (d.properties.name === 'France') return [-9, 66]
+      if (d.properties.name === 'Italy') return [-8, -6]
+      if (d.properties.name === 'Russia') return [5, 385]
+      // Africa
+      if (d.properties.name === 'Madagascar') return [-10, 10]
+      // Asia
+      if (d.properties.name === 'China') return [-16, -8]
+      if (d.properties.name === 'Mongolia') return [-5, 0]
+      if (d.properties.name === 'Pakistan') return [-10, 13]
+      if (d.properties.name === 'India') return [-11, -18]
+      if (d.properties.name === 'Nepal') return [-8, 1]
+      if (d.properties.name === 'Myanmar') return [-12, 0]
+      if (d.properties.name === 'Laos') return [-12, -8]
+      if (d.properties.name === 'Vietnam') return [-12, -4]
+      if (d.properties.name === 'Japan') return [5, 5]
+      // Oceania
+      if (d.properties.name === 'Indonesia') return [0, -5]
+      if (d.properties.name === 'Papua New Guinea') return [-5, -10]
+      if (d.properties.name === 'Australia') return [-15, 0]
+      if (d.properties.name === 'New Zealand') return [-15, 0]
+      // otherwise if not specified
+      return [-10, 0]
+    })
+
+    return tip;
+  }
+
+  updateChart() {
     if (this.props.width === 0 || this.props.height === 0) {
       return;
     }
+    const tip = this.getTip();
     const { data, onSelectedCountryIdChange } = this.props;
-
-    const format = d3.format(',');
 
     const margin = {top: 0, right: 0, bottom: 0, left: 0};
     const width = this.props.width - margin.left - margin.right;
@@ -81,6 +162,7 @@ class Map extends React.Component {
       }
 
       const g = svg.append("g");
+      svg.call(tip)
 
       g.append('g')
         .attr('class', 'countries')
@@ -93,16 +175,19 @@ class Map extends React.Component {
         .style('stroke', 'white')
         .style('opacity', 0.8)
         .style('stroke-width', 0.3)
-        .on('mouseover', function (d) {
+        // tooltips
+        .on('mouseover', function(event, d) {
+          tip.show(d, event)
           d3.select(this)
             .style('opacity', 1)
-            .style('stroke-width', 1);
+            .style('stroke-width', 1)
         })
-        .on('mouseout', function (d) {
+        .on('mouseout', function(event, d) {
+          tip.hide(d, event)
           d3.select(this)
             .style('opacity', 0.8)
-            .style('stroke-width', 0.3);
-        });
+            .style('stroke-width', 0.3)
+        })
 
       const zoom = d3.zoom()
         .scaleExtent([1, 8])
